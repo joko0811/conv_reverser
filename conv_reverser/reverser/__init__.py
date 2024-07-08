a26ca1fb3d4c08b16b11e62becaccbee27549b4e
joko0811 <perry_0096@docomo.ne.jp>
2024-07-08 17:19:32 +0900

    lib

---
+++ conv_reverser/reverser/__init__.py
@@ -0,0 +1,3 @@
+from .s3fd_reverse import s3fd_reverse, s3fd_feature_num_converter
+from .conv_reverse import conv_reverse, net_reverse
+from .vgg_reverse import generate_vgg_params
---
+++ setup.py
@@ -0,0 +1,3 @@
+from setuptools import setup, find_packages
+
+setup(name="conv_reverser", version="0.1", packages=find_packages())
